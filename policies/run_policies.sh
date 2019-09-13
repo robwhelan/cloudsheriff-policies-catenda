841e68e32a8415d8f2c18a463a21623c3f9bc134
Rob Whelan <whelan@gmail.com>
2019-09-13 16:02:06 -0500

    only do stuff for master branch

--- policies/run_policies.sh
+++
@@ -1,3 +0,0 @@
-custodian run -s s3://cloudjanitorz underutilized_ec2.yaml
-custodian run -s s3://cloudjanitorz release_unallocated_eips.yaml
-custodian run -s s3://cloudjanitorz forgotten_sagemaker_notebook.yaml
