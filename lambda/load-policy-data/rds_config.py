50464f69ef9c1dc0e2e8bbd5504c3e1b548706c1
Cloud Sheriff <operations@getcloudsheriff.com>
2019-10-17 20:27:07 -0500

    remove lambda since it is deprecated

--- lambda/load-policy-data/rds_config.py
+++
@@ -1,5 +0,0 @@
-#config file containing credentials for RDS MySQL instance
-db_username = "cloudjanitorz"
-db_password = "cloudjanitorz"
-db_name = "cloudjanitorz"
-rds_instance_endpoint = 'cloudjanitorz.c3cnrieiquqk.us-east-1.rds.amazonaws.com'
--- lambda/mysql/zip-update-test.sh
+++
@@ -1,4 +0,0 @@
-zip -g function.zip app.py rds_config.py
-aws lambda update-function-code --function-name cloudjanitorz-connectmysql --zip-file fileb://function.zip
-aws lambda invoke --function-name cloudjanitorz-connectmysql lambda-results
-atom lambda-results
